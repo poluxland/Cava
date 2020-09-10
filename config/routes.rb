9aa90ae9e0c63cb8007c3b572c56171dac6f536a
Jose Jerez <joseignaciojerez@gmail.com>
2020-09-10 19:30:08 -0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :announcements
+    resources :notifications
+    resources :services
+
+    root to: "users#index"
+  end
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
     authenticate :user, lambda { |u| u.admin? } do
