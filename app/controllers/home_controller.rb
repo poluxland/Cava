6dc40528543c82db87c84c2e9812b5abd8ca96c7
Jose Jerez <joseignaciojerez@gmail.com>
2020-10-22 17:27:02 -0300

    add tables to main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @insumos_orders = InsumosOrder.where("cantidad > 0").last(5)
+    @elementos_orders = ElementosOrder.where("cantidad > 0").last(5)
+    @tools_orders = ToolsOrder.where(status: true).last(5)
   end
 
   def terms
@@ -7,4 +10,6 @@ class HomeController < ApplicationController
 
   def privacy
   end
+
+
 end
