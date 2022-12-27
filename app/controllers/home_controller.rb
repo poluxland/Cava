4bbd2a61c69d8afbfee021bd5a1642e62def7c39
Jose Jerez <joseignaciojerez@gmail.com>
2022-12-27 14:52:08 -0300

    more months in boxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,19 @@ class HomeController < ApplicationController
     @insumos0 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: Date.today.beginning_of_month..Date.today.end_of_month).sum("insumos_orders.cantidad * insumos.valor")
     @insumos1 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 1.month)..((Date.today.end_of_month) - 1.month)).sum("insumos_orders.cantidad * insumos.valor")
     @insumos2 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 2.month)..((Date.today.end_of_month) - 2.month)).sum("insumos_orders.cantidad * insumos.valor")
+    @insumos3 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 3.month)..((Date.today.end_of_month) - 3.month)).sum("insumos_orders.cantidad * insumos.valor")
+    @insumos4 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 4.month)..((Date.today.end_of_month) - 4.month)).sum("insumos_orders.cantidad * insumos.valor")
+    @insumos5 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 5.month)..((Date.today.end_of_month) - 5.month)).sum("insumos_orders.cantidad * insumos.valor")
+    @insumos6 = InsumosOrder.where("insumos_orders.cantidad > ?", 0).joins(:insumo).where(created_at: ((Date.today.beginning_of_month) - 6.month)..((Date.today.end_of_month) - 6.month)).sum("insumos_orders.cantidad * insumos.valor")
     @elementos0 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: Date.today.beginning_of_month..Date.today.end_of_month).sum("elementos_orders.cantidad * elementos.valor")
     @elementos1 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 1.month)..((Date.today.end_of_month) - 1.month)).sum("elementos_orders.cantidad * elementos.valor")
     @elementos2 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 2.month)..((Date.today.end_of_month) - 2.month)).sum("elementos_orders.cantidad * elementos.valor")
+    @elementos3 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 3.month)..((Date.today.end_of_month) - 3.month)).sum("elementos_orders.cantidad * elementos.valor")
+    @elementos4 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 4.month)..((Date.today.end_of_month) - 4.month)).sum("elementos_orders.cantidad * elementos.valor")
+    @elementos5 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 5.month)..((Date.today.end_of_month) - 5.month)).sum("elementos_orders.cantidad * elementos.valor")
+    @elementos6 = ElementosOrder.where("elementos_orders.cantidad > ?", 0).joins(:elemento).where(created_at: ((Date.today.beginning_of_month) - 6.month)..((Date.today.end_of_month) - 6.month)).sum("elementos_orders.cantidad * elementos.valor")
+
+
   end
 
   def ins
